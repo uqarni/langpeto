43f6d68725a45c9da91e8d71907b8abb79139ee9
uqarni <108438834+uqarni@users.noreply.github.com>
2023-03-11 21:28:27 +0000

    got a terminal tarabot working!

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-#call chatgpt with messages and openai API key
-def ideator_gpt(messages, key):
+#generate openai response; returns messages with openai response
+def ideator_gpt(messages):
   import openai
   import os
-
+  key = "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"
   openai.api_key = key
 
   work = "doctor"
@@ -23,10 +23,34 @@ def ideator_gpt(messages, key):
   return messages
 
 
-test = [
-           {"role": "system", "content": "You are a sassy back entrepeneur named Tara who helps people come up with app ideas based on their business or hobby. You often ask clarifying questions to guide them to ideas instead of just giving them ideas."},
-           {"role": "user", "content": "Hi, my job is an architect and I like to go fishing."},
-     ]
-call = ideator_gpt(messages = test, key = "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")
-print(call[-1])
+#prompt user with botresponse in terminal and ask for an input. returns messages with human response
+def terminaltalker(messages):
 
+  botresponse = messages[-1]["content"]
+  userresponse = input(botresponse+"\n")
+  messages.append(
+  {
+    "role": "user",
+    "content": userresponse
+  }
+  )
+  return messages
+
+
+def terminalbot():
+    #initialize message
+    messages = [
+          {"role": "system", "content": "You are a sassy back entrepeneur named Tara who helps people come up with app ideas based on their business or hobby. You ask clarifying questions about their job and hobby, then guide them towards ideas. Make it interactive and shepherd them through the process. If the user is not responding posivitely, switch to asking clarifying questions for a bit before proceeding."},
+          {"role": "assistant", "content": "Hi! This is Tara. Seems like you need help coming up with an idea! Let's do this. First, what's your job?"}
+          ]
+    while True:
+       messages = terminaltalker(messages)
+       if messages[-1]["content"] == "exit()":
+          break
+       ideator_gpt(messages)
+
+
+
+
+      
+  
--- streamlitapp.py
+++ streamlitapp.py
@@ -1,30 +1,48 @@
 import streamlit as st
-import os
-import openai
+import webbrowser
+
 
 st.title("Ideator Bot")
 
-#ask if they have an idea already
+#ask if they have an idea already and save the user's answer
+question = "Do you have an app idea already?"
+answer_options = ["Yes, I have an app idea already.", "No, I don't have an app idea already."]
+answer = st.radio(question, answer_options)
 
-#ask their job
+# conditional component once they click Next Step
+if st.button ("Next Step"):
+    #if they chose yes, take them to an external site
+    if answer == answer_options[0]:
+        webbrowser.open_new_tab("https://www.youtube.com/watch?v=xvFZjo5PgG0")
+    #if they chose no, ask them what their job and type thier job to them
+    if answer == answer_options[1]:
+        st.success("No problem! Let's ideate. First, what job do you work?")
+        job = st.text_input("Chat window", key = "job_input")
+        
+        if st.session_state.job_input:
+            if st.session_state.job_input.endswith("\n"):
+                st.write("your job is "+job)
+        
+
+
 
 #ask their hobby
 
 #ask 
 
-if uploaded_file is not None:
-    with open("text.txt","wb") as f:
-        f.write(uploaded_file.getbuffer())
-    st.success("File uploaded")
+# if uploaded_file is not None:
+#     with open("text.txt","wb") as f:
+#         f.write(uploaded_file.getbuffer())
+#     st.success("File uploaded")
 
-user_question = st.text_input(
-    "Enter Your Question : ",
-    placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
-)
+# user_question = st.text_input(
+#     "Enter Your Question : ",
+#     placeholder = "Cyanobacteria can perform photosynthetsis , are they considered as plants?",
+# )
 
-if st.button("Tell me about it", type = "primary"):
-    loader = TextLoader('text.txt')
-    index = VectorstoreIndexCreator().from_loaders([loader])
-    query = user_question
-    answer = index.query_with_sources(query)
-    st.success(answer['answer'])
+# if st.button("Tell me about it", type = "primary"):
+#     loader = TextLoader('text.txt')
+#     index = VectorstoreIndexCreator().from_loaders([loader])
+#     query = user_question
+#     answer = index.query_with_sources(query)
+#     st.success(answer['answer'])
